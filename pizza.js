0f3b528a0251a39ac30aedbb31ec7144949de093
Danoslo 255 <68803448+777dan@users.noreply.github.com>
2021-10-27 19:20:25 +0300

    Add files via upload

--- pizza.js
+++ pizza.js
@@ -1,7 +1,7 @@
 let question1 = prompt("Введите 1, если хотите пиццу с грибами. Введите 2, если хотите пиццу с курицей и ананасом. Введите 3, если хотите пиццу с сыром. Введите 4, если хотите пиццу с помидорами и колбасой.");
 let question2 = prompt("Сколько штук пицц вы хотите?");
-let str;
 function orderingPizza(arg1, arg2) {
+    let str;
     switch (arg1) {
         case "1":
             question1 = "пицца с грибами";
@@ -19,22 +19,23 @@ function orderingPizza(arg1, arg2) {
             break;
 
     }
-    if (!arg2.trim()) {
-        question2 = 1;
-        str = "Ваш заказ: " + question1 + " (" + question2 + " шт.)";
-    }
-    if (!arg1.trim() && !arg2.trim()) {
-        question2 = 1;
-        question1 = "Фирмення пицца"
-        str = "Ваш заказ: " + question1 + " (" + question2 + " шт.)";
-    }
-    if (question1 > 4 && question2 < 1) {
+    if (arg1.trim() || arg2.trim() && isNaN(arg1) || isNaN(arg2) || arg1 > 4 || arg1 < 1 || arg2 < 1) {
         str = "Произошла ошибка, повторите заказ.";
     }
-    else {
+    if (!arg2.trim()) {
+        if (!arg1.trim()) {
+            question2 = 1;
+            question1 = "Фирмення пицца"
+            str = "Ваш заказ: " + question1 + " (" + question2 + " шт.)";
+        } else {
+            question2 = 1;
+            str = "Ваш заказ: " + question1 + " (" + question2 + " шт.)";
+        }
+    }
+    if (arg1 >= 1 && arg1 <= 4 && arg2 >= 1) {
         str = "Ваш заказ: " + question1 + " (" + question2 + " шт.)";
     }
-    alert(str);
     return str;
 }
 let order = orderingPizza(question1, question2);
+alert(order);
